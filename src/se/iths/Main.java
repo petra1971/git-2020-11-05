2c09f362a559cde11bf7393e66477dd76e55a084
Petra Andreasson <petra.andreasson@iths.se>
2020-11-05 11:05:57 +0100

    another feature added

--- src/se/iths/Main.java
+++ src/se/iths/Main.java
@@ -2,6 +2,6 @@ package se.iths;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fourth version with a new feature");
+        System.out.println("Fourth version with another new feature");
     }
 }
